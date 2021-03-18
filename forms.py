ef7c78cdcc618355fd03c74755db14bdbb4b1153
idrissa-mgs <idrissamagassa@gmail.com>
2021-03-18 22:15:11 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
 class EmotionForm(FlaskForm):
-    text_sentiment = StringField("Hi, tell me how do you feel ?", validators=[DataRequired()])
+    TextAreaField = StringField("Hi, tell me how do you feel ?", validators=[DataRequired()])
     
     submit = SubmitField("Get the emotion")
